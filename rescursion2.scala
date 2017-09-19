d20b2c72beeae926589ec97c4f31f424ba4bd2e3
hrpradeep32 <hrpradeep32@gmail.com>
2017-09-19 23:26:50 +0530

    Add files via upload

---
+++ rescursion2.scala
@@ -0,0 +1,20 @@
+
+
+object rescursion2 {
+  def fib2( n : Int ) : Int = {
+  var a = 0
+  var b = 1
+  var i = 0	  
+ 
+  while( i < n ) {
+    val c = a + b
+    a = b
+    b = c
+    i = i + 1
+  } 
+  return a
+}
+
+
+
+}
